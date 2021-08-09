c7fcb69854b03f232c7fd05a564d0e278789b52c
Steven Sim <stevensim226@gmail.com>
2021-08-09 20:52:46 +0700

    Finish frontend and add env vars

--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,20 @@
+import os
 from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "frontend/index.html")
+    return render(request, "frontend/index.html", {
+        "backend_url": os.environ["BACKEND_URL"]
+    })
 
 def detail(request, status_uuid):
-    return render(request, "frontend/detail.html", {"status_uuid" : status_uuid})
+    return render(request, "frontend/detail.html", {
+        "status_uuid" : status_uuid,
+        "backend_url": os.environ["BACKEND_URL"]
+    })
 
 def edit(request, status_uuid):
-    return render(request, "frontend/edit.html", {"status_uuid" : status_uuid})
+    return render(request, "frontend/edit.html", {
+        "status_uuid" : status_uuid,
+        "backend_url": os.environ["BACKEND_URL"]
+    })
